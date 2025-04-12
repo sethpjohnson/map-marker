61024525f3b10c88b8961ee9a44d4f188242e6c4
Seth Johnson <spjohnson@gmail.com>
2025-04-12 11:21:17 -0400

    github pages build

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,10 @@ const config = {
 			assets: 'build',
 			fallback: 'index.html',
 			precompress: false
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/map-marker' : ''
+		}
 	}
 };
 
